ea275602322ef9bab4a56c838cdcef7800966081
peterjaap <peterjaap@elgentos.nl>
2018-09-27 11:15:48 +0200

    Refactored pushing items to prewarm queue to seperate notifier class, added plugin for mass update attributes call

--- Observer/ProductSaveAfter.php
+++ Observer/ProductSaveAfter.php
@@ -2,41 +2,17 @@
 
 namespace Elgentos\LargeConfigProducts\Observer;
 
-use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
-
-use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable as ConfigurableResourceModel;
+use Elgentos\LargeConfigProducts\Model\PublisherNotifier;
 use Magento\Framework\Event\ObserverInterface;
-use Rcason\Mq\Api\PublisherInterface;
-use Magento\Framework\Module\Manager as ModuleManager;
 
 class ProductSaveAfter implements ObserverInterface
 {
     /**
-     * @var PublisherInterface
+     * ProductSaveAfter constructor.
+     * @param PublisherNotifier $publisherNotifier
      */
-    private $publisher;
-    /**
-     * @var ConfigurableResourceModel
-     */
-    private $configurableResourceModel;
-    /**
-     * @var ModuleManager
-     */
-    private $moduleManager;
-
-    /**
-     * @param PublisherInterface $publisher
-     * @param ConfigurableResourceModel $configurableResourceModel
-     * @param ModuleManager $moduleManager
-     */
-    public function __construct(
-        PublisherInterface $publisher,
-        ConfigurableResourceModel $configurableResourceModel,
-        ModuleManager $moduleManager
-    ) {
-        $this->publisher = $publisher;
-        $this->configurableResourceModel = $configurableResourceModel;
-        $this->moduleManager = $moduleManager;
+    public function __construct(PublisherNotifier $publisherNotifier) {
+        $this->publisherNotifier = $publisherNotifier;
     }
 
     /**
@@ -45,15 +21,6 @@ class ProductSaveAfter implements ObserverInterface
     public function execute(
         \Magento\Framework\Event\Observer $observer
     ) {
-        if ($this->moduleManager->isEnabled('Rcason_Mq')) {
-            if ($observer->getProduct()->getTypeId() == Configurable::TYPE_CODE) {
-                $this->publisher->publish('lcp.product.prewarm', $observer->getProduct()->getId());
-            } elseif ($observer->getProduct()->getTypeId() == 'simple') {
-                $parentIds = $this->configurableResourceModel->getParentIdsByChild($observer->getProduct()->getId());
-                foreach ($parentIds as $parentId) {
-                    $this->publisher->publish('lcp.product.prewarm', $parentId);
-                }
-            }
-        }
+        $this->publisherNotifier->notify([$observer->getProduct()->getId()]);
     }
 }
---
+++ Plugin/Product/Action/AfterUpdateAttributesPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Elgentos\LargeConfigProducts\Plugin\Product\Action;
+
+use Elgentos\LargeConfigProducts\Model\PublisherNotifier;
+use Magento\Catalog\Model\Product\Action as ProductAction;
+
+class AfterUpdateAttributesPlugin {
+
+    /**
+     * AfterUpdateAttributesPlugin constructor.
+     * @param PublisherNotifier $publisherNotifier
+     */
+    public function __construct(PublisherNotifier $publisherNotifier) {
+        $this->publisherNotifier = $publisherNotifier;
+    }
+
+    /**
+     * @param ProductAction $subject
+     * @param ProductAction $action
+     * @param $productIds
+     * @param $attrData
+     * @param $storeId
+     * @return ProductAction
+     */
+    public function afterUpdateAttributes(
+        ProductAction $subject,
+        ProductAction $action,
+        $productIds,
+        $attrData,
+        $storeId
+    ) {
+        $this->publisherNotifier->notify($productIds);
+
+        return $action;
+    }
+
+}
